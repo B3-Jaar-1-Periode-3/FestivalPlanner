7720f1a5cc92129a2dd6e120a259e0bd9e9d0b7b
Richard/NotDutchy <richard.elean@gmail.com>
2022-02-10 13:50:12 +0100

    Added initial pane and launch to the class eventGUI.

--- src/Agenda/EventGUI.java
+++ src/Agenda/EventGUI.java
@@ -1,4 +1,28 @@
 package Agenda;
 
-public class EventGUI {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.PrintWriter;
+
+public class EventGUI extends Application {
+    private BorderPane mainPane;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        mainPane = new BorderPane();
+
+        Scene scene = new Scene(mainPane);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(EventGUI.class);
+    }
+
 }
