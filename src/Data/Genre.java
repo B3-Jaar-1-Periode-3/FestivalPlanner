288ddb9224fc1ab2268e63de52bba1ef84e2a631
Daan Tukker <d.tukker@student.avans.nl>
2022-02-10 14:36:56 +0100

    Arraylist Genre added to genre class

--- src/Data/Genre.java
+++ src/Data/Genre.java
@@ -1,5 +1,23 @@
 package Data;
 
+import java.util.ArrayList;
+
 public class Genre {
 
+    ArrayList<String> genre;
+
+    public Genre() {
+        this.genre = new ArrayList<>();
+        genre.add("Pop");
+        genre.add("Rock");
+        genre.add("Rap");
+        genre.add("Hip-Hop");
+        genre.add("Country");
+        genre.add("R&B");
+        genre.add("Folk");
+        genre.add("Jazz");
+        genre.add("Heavy metal");
+        genre.add("EDM");
+        genre.add("Soul");
+    }
 }
