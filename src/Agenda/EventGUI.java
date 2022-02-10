3334e0cf5c74f07be0663fd96fd2374a3bf89c07
Richard/NotDutchy <richard.elean@gmail.com>
2022-02-10 14:31:24 +0100

    Added comboBox for artists. Also added preferred size for GUI window.

--- src/Agenda/EventGUI.java
+++ src/Agenda/EventGUI.java
@@ -1,19 +1,42 @@
 package Agenda;
 
+import Data.Artist;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.awt.*;
-import java.io.PrintWriter;
+import java.util.ArrayList;
+
 
 public class EventGUI extends Application {
     private BorderPane mainPane;
+    private VBox vBox;
+    private HBox hBox;
+    private ComboBox<String> artists;
+    private ArrayList<Artist> artistArrayList;
 
     @Override
     public void start(Stage stage) throws Exception {
+        artistArrayList = new ArrayList<>();
         mainPane = new BorderPane();
+        hBox = new HBox();
+        vBox = new VBox();
+        artists = new ComboBox<>();
+
+        for (Artist artist : artistArrayList) {
+            artists.getItems().add(artist.getName());
+        }
+
+        mainPane.setPrefSize(1600,1000);
+        mainPane.setLeft(hBox);
+        hBox.setSpacing(20);
+        hBox.getChildren().addAll(new Label("Artiest:"), vBox);
+        vBox.getChildren().addAll(artists);
 
         Scene scene = new Scene(mainPane);
 
@@ -24,5 +47,4 @@ public class EventGUI extends Application {
     public static void main(String[] args) {
         launch(EventGUI.class);
     }
-
 }
