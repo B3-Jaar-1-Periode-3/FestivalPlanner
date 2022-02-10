a681710feab785134a7f1a8d679c9511c150ebb0
Michelle <mj.vandesande1@student.avans.nl>
2022-02-10 12:15:51 +0100

    making GUI base and inporting libs

--- src/Agenda/AgendaGUI.java
+++ src/Agenda/AgendaGUI.java
@@ -1,6 +1,49 @@
 package Agenda;
 
-import javafx.scene.canvas.Canvas;
 
-public class AgendaGUI extends Canvas {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import org.jfree.fx.FXGraphics2D;
+import org.jfree.fx.ResizableCanvas;
+
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+
+public class AgendaGUI extends Application {
+
+    private ResizableCanvas canvas;
+
+    @Override
+    public void start(Stage stage)  {
+
+        BorderPane mainPane = new BorderPane();
+        canvas = new ResizableCanvas(g -> draw(g), mainPane);
+        mainPane.setCenter(canvas);
+        FXGraphics2D g2d = new FXGraphics2D(canvas.getGraphicsContext2D());
+
+
+        stage.setScene(new Scene(mainPane, 1920, 1080));
+        stage.setTitle("Agenda");
+        stage.show();
+        draw(g2d);
+    }
+
+    public void init() {
+    }
+
+    public void draw(FXGraphics2D graphics) {
+        graphics.setTransform(new AffineTransform());
+        graphics.setBackground(Color.white);
+        graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+
+
+    }
+
+    public static void main(String[] args) {
+        launch(AgendaGUI.class);
+    }
+
 }
