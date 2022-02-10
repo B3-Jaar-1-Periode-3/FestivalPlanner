b8c416981e90c0397d152ea1c72936b6b73bb3d9
Michelle <mj.vandesande1@student.avans.nl>
2022-02-10 14:53:01 +0100

    added method write and writed stages

--- src/Agenda/AgendaGUI.java
+++ src/Agenda/AgendaGUI.java
@@ -38,15 +38,22 @@ public class AgendaGUI extends Application {
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
         graphics.translate(this.canvas.getWidth() / 2, this.canvas.getHeight() / 2);
-        graphics.scale(1, -1);
+       // graphics.scale(1, -1);
 
-        graphics.setStroke(drawLine(10));
-        graphics.drawLine(-760,1080,-760,-1080);
-        graphics.drawLine(-1800,380,1800,380);
-        graphics.drawLine(-1800,180,1800,180);
-        graphics.drawLine(-1800,-20,1800,-20);
-        graphics.drawLine(-1800,-220,1800,-220);
-        graphics.drawLine(-1800,-420,1800,-420);
+        drawGrit(graphics);
+       // graphics.translate(this.canvas.getWidth()*2,this.canvas.getHeight()*2);
+
+        int x = -920;
+        int y= 290;
+
+        Font font = new Font("Serif", Font.PLAIN, 30);
+        Shape shape = font.createGlyphVector(graphics.getFontRenderContext(), "Main Stage").getOutline();
+        drawText(graphics,-920,290,"main stage");
+        drawText(graphics,-920,90,"substage 1");
+        drawText(graphics,-920,-110,"substage 2");
+        drawText(graphics,-920,-310,"Substage 3");
+
+
 
 
 
@@ -60,8 +67,24 @@ public class AgendaGUI extends Application {
         return s;
     }
 
+    //maakt de lijnen die op het canvas staan.
+    public void drawGrit(FXGraphics2D graphics){
+        graphics.setStroke(drawLine(10));
+        graphics.drawLine(-760,1080,-760,-1080);
+        graphics.drawLine(-1800,380,1800,380);
+        graphics.drawLine(-1800,180,1800,180);
+        graphics.drawLine(-1800,-20,1800,-20);
+        graphics.drawLine(-1800,-220,1800,-220);
+        graphics.drawLine(-1800,-420,1800,-420);
+    }
+
+    public void drawText(FXGraphics2D graphics2D,int x, int y, String name){
+        graphics2D.drawString(name,x, -y);
+    }
+
     public static void main(String[] args) {
         launch(AgendaGUI.class);
     }
+
 
 }
