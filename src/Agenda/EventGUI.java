714ba5b28f5808835a87341a35e5f50d3483eae6
Richard/NotDutchy <richard.elean@gmail.com>
2022-02-10 14:49:01 +0100

    Added method to fill the arraylist with artists.

--- src/Agenda/EventGUI.java
+++ src/Agenda/EventGUI.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -32,16 +33,24 @@ public class EventGUI extends Application {
             artists.getItems().add(artist.getName());
         }
 
-        mainPane.setPrefSize(1600,1000);
+        artists.setPrefSize(100,30);
+        Label labelArtist = new Label("Artiest:");
+        labelArtist.setFont(Font.font(20));
+
+        mainPane.setPrefSize(1000,800);
         mainPane.setLeft(hBox);
         hBox.setSpacing(20);
-        hBox.getChildren().addAll(new Label("Artiest:"), vBox);
+        hBox.getChildren().addAll(labelArtist, vBox);
         vBox.getChildren().addAll(artists);
 
         Scene scene = new Scene(mainPane);
 
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void setArtistArrayList(ArrayList<Artist> artists) {
+        this.artistArrayList = artists;
     }
 
     public static void main(String[] args) {
