87e87f0d869959e546f29de1b4af843a0dca314c
Richard/NotDutchy <richard.elean@gmail.com>
2022-02-17 11:19:44 +0100

    Added combobox and label for podium.

--- src/Agenda/EventGUI.java
+++ src/Agenda/EventGUI.java
@@ -1,6 +1,7 @@
 package Agenda;
 
 import Data.Artist;
+import Data.Genre;
 import Data.Podium;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -17,33 +18,51 @@ import java.util.ArrayList;
 
 public class EventGUI extends Application {
     private BorderPane mainPane;
-    private VBox vBox;
+    private VBox vBoxLabels;
+    private VBox vBoxComboboxes;
     private HBox hBox;
     private ComboBox<String> artists;
+    private ComboBox<String> podiaBox;
     private ArrayList<Artist> artistArrayList;
     private ArrayList<Podium> podiumArrayList;
+    private ArrayList<Genre> genreArrayList;
+
 
     @Override
     public void start(Stage stage) throws Exception {
         artistArrayList = new ArrayList<>();
         mainPane = new BorderPane();
         hBox = new HBox();
-        vBox = new VBox();
+        vBoxLabels = new VBox();
+        vBoxComboboxes = new VBox();
         artists = new ComboBox<>();
+        podiaBox = new ComboBox<>();
+        podiumArrayList = new ArrayList<>();
+
+
+        podiumArrayList.add(new Podium(1));
+        artistArrayList.add(new Artist("Agt"));
 
         for (Artist artist : artistArrayList) {
             artists.getItems().add(artist.getName());
         }
 
+        for (Podium podium : podiumArrayList) {
+            podiaBox.getItems().add(podium.toString());
+        }
+
         artists.setPrefSize(100,30);
         Label labelArtist = new Label("Artiest:");
         labelArtist.setFont(Font.font(20));
+        Label labelPodium = new Label("Podium:");
+        labelPodium.setFont(Font.font(20));
 
         mainPane.setPrefSize(1000,800);
         mainPane.setLeft(hBox);
         hBox.setSpacing(20);
-        hBox.getChildren().addAll(labelArtist, vBox);
-        vBox.getChildren().addAll(artists);
+        hBox.getChildren().addAll(vBoxLabels, vBoxComboboxes);
+        vBoxLabels.getChildren().addAll(labelArtist, labelPodium);
+        vBoxComboboxes.getChildren().addAll(artists, podiaBox);
 
         Scene scene = new Scene(mainPane);
 
@@ -58,6 +77,10 @@ public class EventGUI extends Application {
 
     public void setPodiumArrayList(ArrayList<Podium> podiums) {
         this.podiumArrayList = podiums;
+    }
+
+    public void setGenreList(ArrayList<Genre> genres) {
+        this.genreArrayList = genres;
     }
 
     public static void main(String[] args) {
