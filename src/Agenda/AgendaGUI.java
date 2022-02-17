b3aff956af224b34b5d9326d58298a0f20e079ad
Michelle <mj.vandesande1@student.avans.nl>
2022-02-17 12:27:44 +0100

    added draw text and draw time

--- src/Agenda/AgendaGUI.java
+++ src/Agenda/AgendaGUI.java
@@ -17,6 +17,7 @@ import javafx.scene.layout.BorderPane;
 
 import java.awt.*;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Line2D;
 
 public class AgendaGUI extends Application {
 
@@ -95,16 +96,18 @@ public class AgendaGUI extends Application {
     public void draw(FXGraphics2D graphics) {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
+        canvas.setWidth(1920.0);
+        canvas.setHeight(2023.0);
+
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
         graphics.translate(this.canvas.getWidth() / 2, this.canvas.getHeight() / 2);
-        // graphics.scale(1, -1);
 
         drawGrit(graphics);
+        drawText(graphics);
+        drawTime(graphics);
+        System.out.println("with :"+canvas.getWidth());
+        System.out.println("hitht: "+ canvas.getHeight());
 
-        drawText(graphics,-920,290,"main stage");
-        drawText(graphics,-920,90,"substage 1");
-        drawText(graphics,-920,-110,"substage 2");
-        drawText(graphics,-920,-310,"Substage 3");
 
     }
 
@@ -118,14 +121,37 @@ public class AgendaGUI extends Application {
 
     //maakt de lijnen die op het canvas staan.
     public void drawGrit(FXGraphics2D graphics){
-        graphics.setStroke(drawLine(10));
-        graphics.drawLine(-760,1080,-760,-1080);
-        graphics.drawLine(-1800,380,1800,380);
-        graphics.drawLine(-1800,180,1800,180);
-        graphics.drawLine(-1800,-20,1800,-20);
-        graphics.drawLine(-1800,-220,1800,-220);
+
+        graphics.setStroke(drawLine(5));
+        graphics.drawLine(-820,1080,-820,-1080);
+        graphics.drawLine(-365,-550,-365,500);
+        graphics.drawLine(90,-550,90,500);
+        graphics.drawLine(545,-550,545,500);
+        graphics.drawLine(1000,-550,1000,500);
+
+
+
         graphics.drawLine(-1800,-420,1800,-420);
     }
+
+    public void drawText(FXGraphics2D graphics2D){
+        drawText(graphics2D,-800,480,"main stage");
+        drawText(graphics2D,-345,480,"substage 1");
+        drawText(graphics2D,110,480,"substage 2");
+        drawText(graphics2D,565,480,"Substage 3");
+    }
+
+    public void drawTime(FXGraphics2D graphics2D){
+        int y=400 ;
+        for (int time = 0; time < 24; time++) {
+            if (time<10){
+                drawText(graphics2D,-900,y,"0"+time+".00");
+            }
+            else { drawText(graphics2D,-900,y,time+".00");}
+            y-=35;
+        }
+    }
+
 
     public void drawText(FXGraphics2D graphics2D,int x, int y, String name){
         graphics2D.drawString(name,x, -y);
