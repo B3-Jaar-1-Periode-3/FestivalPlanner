736f24581d3e87801a06e1ad222d8b93d39d07eb
Rae <ruisizheng0@gmail.com>
2022-02-17 14:13:30 +0100

    Auto stash before cherry pick of "Added submenu's and updated comments"

--- src/Agenda/AgendaGUI.java
+++ src/Agenda/AgendaGUI.java
@@ -2,9 +2,11 @@ package Agenda;
 
 
 import javafx.application.Application;
+import javafx.scene.*;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
@@ -13,8 +15,10 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
 
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Line2D;
@@ -31,18 +35,21 @@ public class AgendaGUI extends Application {
         mainPane.setCenter(canvas);
         FXGraphics2D g2d = new FXGraphics2D(canvas.getGraphicsContext2D());
 
+        // Single buttons
+        Button help = new Button("Help");
 
-        Menu menu = new Menu("Menu");
 
-        // load file (always first on the list)
+        // File menu and submenu's
+        Menu file = new Menu("File");
+        Menu fileMenu = new Menu("File");
         MenuItem loadFile = new MenuItem("Load file");
-        menu.getItems().add(loadFile);
+        MenuItem savePlanning = new MenuItem("Save planning");
+        fileMenu.getItems().add(loadFile);
+        fileMenu.getItems().add(savePlanning);
+        file.getItems().add(fileMenu);
 
-        // save planning menu
-        MenuItem saveEdit = new MenuItem("Save planning");
-        menu.getItems().add(saveEdit);
-
-        // edit menu & sub menu
+        // edit menu & sub menu's
+        Menu edit = new Menu("Edit");
         Menu editMenu = new Menu("Edit");
         MenuItem editEvent = new MenuItem("Edit event");
         MenuItem editArtist = new MenuItem("Edit artist");
@@ -50,9 +57,10 @@ public class AgendaGUI extends Application {
         editMenu.getItems().add(editEvent);
         editMenu.getItems().add(editArtist);
         editMenu.getItems().add(editGenre);
-        menu.getItems().add(editMenu);
+        edit.getItems().add(editMenu);
 
-        // save menu & sub menu
+        // save menu & sub menu's
+        Menu save = new Menu("Save");
         Menu saveMenu = new Menu("Save");
         MenuItem saveEvent = new MenuItem("Save event");
         MenuItem saveArtist = new MenuItem("Save artist");
@@ -60,9 +68,10 @@ public class AgendaGUI extends Application {
         saveMenu.getItems().add(saveEvent);
         saveMenu.getItems().add(saveArtist);
         saveMenu.getItems().add(saveGenre);
-        menu.getItems().add(saveMenu);
+        save.getItems().add(saveMenu);
 
-        // delete menu & sub menu
+        // delete menu & sub menu's
+        Menu delete = new Menu("Delete");
         Menu deleteMenu = new Menu("Delete");
         MenuItem deleteEvent = new MenuItem("Delete event");
         MenuItem deleteArtist = new MenuItem("Delete artist");
@@ -70,19 +79,23 @@ public class AgendaGUI extends Application {
         deleteMenu.getItems().add(deleteEvent);
         deleteMenu.getItems().add(deleteArtist);
         deleteMenu.getItems().add(deleteGenre);
-        menu.getItems().add(deleteMenu);
+        delete.getItems().add(deleteMenu);
 
-
-        // help menu (always last on the list)
-        MenuItem helpMenu = new MenuItem("Help");
-        menu.getItems().add(helpMenu);
 
         // all menus together
-        MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().add(menu);
+        MenuBar fileMenuBar = new MenuBar();
+        MenuBar editMenuBar = new MenuBar();
+        MenuBar saveMenuBar = new MenuBar();
+        MenuBar deleteMenuBar = new MenuBar();
 
+        fileMenuBar.getMenus().add(fileMenu);
+        editMenuBar.getMenus().add(editMenu);
+        saveMenuBar.getMenus().add(saveMenu);
+        deleteMenuBar.getMenus().add(deleteMenu);
 
-        mainPane.setTop(menuBar);
+        HBox menus = new HBox(fileMenuBar, editMenuBar, saveMenuBar, deleteMenuBar, help);
+        mainPane.setTop(menus);
+        mainPane.setPrefSize(800,1000);
         stage.setScene(new Scene(mainPane, 1920, 1080));
         stage.setTitle("Agenda");
         stage.show();
